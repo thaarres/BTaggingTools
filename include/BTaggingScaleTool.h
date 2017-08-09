479da420564029168141c08fdc077a0c9e07e2c2
thaarres <thea.aarrestad@cern.ch>
2017-08-09 14:22:53 +0200

    Forgot header file

--- include/BTaggingScaleTool.h
+++ include/BTaggingScaleTool.h
@@ -32,11 +32,11 @@ class BTaggingScaleTool : public SToolBase {
 
   double getScaleFactor( const UZH::Jet& jet, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory = "jet");
   
-  double getPrunedSubjetScaleFactor( const UZH::Jet& jet, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory = "subjet_pruned" );
+  double getSoftdropSubjetScaleFactor( const UZH::Jet& jet, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory = "subjet_softdrop" );
 
   double getScaleFactor( const UZH::JetVec& vJets, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory = "jet" );
   
-  double getPrunedSubjetScaleFactor( const UZH::JetVec& vJets, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory = "subjet_pruned" );
+  double getSoftdropSubjetScaleFactor( const UZH::JetVec& vJets, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory = "subjet_softdrop" );
  
   /// function for veto
   double getScaleFactor_veto( const double& pt, const double& eta, const int& flavour, bool isTagged, const double& sigma_bc = 0., const double& sigma_udsg = 0., const TString& jetCategory_veto = "jet_ak4" );
@@ -57,7 +57,7 @@ class BTaggingScaleTool : public SToolBase {
   void fillEfficiencies_veto( const UZH::JetVec& vJets );
   
   /// function to fill subjet b-tagging efficiencies
-  void fillPrunedSubjetEfficiencies( const UZH::JetVec& vJets );
+  void fillSoftdropSubjetEfficiencies( const UZH::JetVec& vJets );
   
   /// function to read in b-tagging efficiencies
   void readEfficiencies();
@@ -92,6 +92,9 @@ class BTaggingScaleTool : public SToolBase {
   std::string m_csvFile_veto;
   std::string m_measurementType_udsg;
   std::string m_measurementType_bc;
+  std::string Clemens;
+  std::string m_measurementType_veto_udsg;
+  std::string m_measurementType_veto_bc;
   std::string m_effHistDirectory;
   std::string m_effFile;
   std::string m_effFile_veto;
@@ -100,8 +103,10 @@ class BTaggingScaleTool : public SToolBase {
   std::vector<TString> m_flavours;
   
   std::map<std::string, double> wpCuts; // could have a function to set these
+
   double currentWorkingPointCut;
   double currentWorkingPointCut_veto;
+
   std::map< std::string, TH2F > m_effMaps;
   std::map< std::string, TH2F > m_effMaps_veto;
 
